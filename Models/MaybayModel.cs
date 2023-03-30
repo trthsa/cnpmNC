060b293cf3411429963ca2e94be23c5ed8aa14e5
qqphong2001 <74895683+qqphong2001@users.noreply.github.com>
2023-03-30 10:59:34 +0700

    connect DB

---
+++ Models/MaybayModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BanVeFlightAPI.Models
+{
+    public class MaybayModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
